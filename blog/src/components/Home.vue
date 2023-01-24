ceccc293db805d38f60da589cea39110ecbf112a
Jaya Vishwakarma <jayavish423@gmail.com>
2023-01-24 22:47:39 +0530

    add properties using funtion and data

--- blog/src/components/Home.vue
+++ blog/src/components/Home.vue
@@ -1,9 +1,22 @@
 <template>
 <h1>Home Components {{10+20}}</h1>
+<h1>Home Components {{"Hello".length}}</h1>
+<h1>Email{{email}}</h1>
+<h1>Mobile{{mobile}}</h1>
+<h1>Name{{getName()}}</h1>
 </template>
 <script>
 export default {
-    name:'Home'
+    name:'Home',
+    data(){
+        return{
+            email:"jaya@gmail.com",
+            mobile:86789867,
+            getName:function(){
+                return "Jaya Vishwakarma"
+            }
+        }
+    }
 }
 </script>
 <style scoped>
