7241bc20f7279b7a060896f57f5514207b315292
Jaya Vishwakarma <jayavish423@gmail.com>
2023-01-25 00:26:31 +0530

    events click and create counter  also button

--- blog/src/components/Button.vue
+++ blog/src/components/Button.vue
@@ -1,8 +1,31 @@
 <template>
-<h1>button</h1>
+    <h1 v-on:mousemove="getConsole()">onmouse</h1>
+<button v-on:click="getData('button 1 clikked successs')">click me</button>
+<button v-on:click="getData('button 2 clikked successs')">click me</button>
+<button v-on:dblclick="getData('button dobule clikked successs')">click me</button>
+<h1>{{count}}</h1>
+<button v-on:click="getCount('button 1 clikked successs')">click me</button>
+<button v-on:click="getCounts('button 2 clikked successs')">click me</button>
 </template>
 <script>
 export default {
-    name:'Button'
+    name:'Button',
+    data(){
+return{
+    count:0
+}
+    },
+    methods:{
+        getData(data){
+            alert(data)
+        },getConsole(){
+            console.log("function run sucess")
+        },
+        getCount(){
+            this.count = this.count +1
+        },getCounts(){
+            this.count=this.count - 1
+        }
+    }
 }
 </script>
