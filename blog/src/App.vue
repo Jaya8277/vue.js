d2dd0a98f950f2b639d85b631425407399f42f60
Jaya Vishwakarma <jayavish423@gmail.com>
2023-01-24 21:54:13 +0530

    make first components in vue

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,20 +1,23 @@
 <template>
 <img alt="Vue logo" src="./assets/logo.png">
 <HelloWorld msg="Welcome to Your Vue.js App" />
+<Home/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        HelloWorld,
+        Home
     }
 }
 </script>
 
-<style>
+<!-- <style>
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -23,4 +26,4 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
 }
-</style>
+</style> -->
