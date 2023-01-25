2d098a8a661fad879f1f4aaddf9ad7cb29d503a6
Jaya Vishwakarma <jayavish423@gmail.com>
2023-01-25 13:50:17 +0530

    towway binding

--- blog/src/components/Button.vue
+++ blog/src/components/Button.vue
@@ -5,14 +5,17 @@
 <button v-on:dblclick="getData('button dobule clikked successs')">click me</button>
 <h1>{{count}}</h1>
 <button v-on:click="getCount('button 1 clikked successs')">click me</button>
-<button v-on:click="getCounts('button 2 clikked successs')">click me</button>
+<button v-on:click="getCounts('button 2 clikked successs')">click me</button><br/><br/>
+<input type="text" v-model="count1"/>
+<h2>{{count1}}</h2>
 </template>
 <script>
 export default {
     name:'Button',
     data(){
 return{
-    count:0
+    count:0,
+    count1:10
 }
     },
     methods:{
